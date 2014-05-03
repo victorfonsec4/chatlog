892dad8d468eb0044263141c062436a1da566ebb
Victor <victor@victor-RF511-RF411-RF711.(none)>
2014-05-03 02:07:10 -0300

    working stuff

--- flask_app.py
+++ flask_app.py
@@ -5,17 +5,39 @@ from flask import Flask
 from flask import render_template
 from flask.ext.social import Social
 from flask.ext.social.datastore import SQLAlchemyConnectionDatastore
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+from sqlalchemy import *
 
 app = Flask(__name__)
-app.config['SOCIAL_FACEBOOK'] =
-{
+app.config['SOCIAL_FACEBOOK'] = {
 	'consumer_key': 1383224128630529,
-	'consumer_secret': 471c63bffbd3e3da00504a50035cd67b
+	'consumer_secret': '471c63bffbd3e3da00504a50035cd67b'
 }
 
 app.config['SECURITY_POST_LOGIN'] = '/profile'
+app.secret_key = 'ahsdijasdijaijasdi'
 
 db = SQLAlchemy(app)
+
+roles_users = db.Table('roles_users',
+        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+        db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
 
 class Connection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,7 +56,7 @@ Social(app, SQLAlchemyConnectionDatastore(db, Connection))
 
 @app.route('/')
 def chatlog():
-    return render_template('iframe.html')
+    return render_template('login.html')
 
 @app.route('/profile')
 @login_required
@@ -43,5 +65,11 @@ def profile():
         'profile.html',
         content='Profile Page',
         facebook_conn=social.facebook.get_connection())
+
+@app.route('/index')
+def page2():
+	return render_template('index.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
