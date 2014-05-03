967b82a0dcf5630afc8ad1f0d974f47d01f6a4c5
Claudionor <claudiinho94@gmail.com>
2014-05-03 02:04:01 -0300

    autocomplete

--- flask_app.py
+++ flask_app.py
@@ -6,9 +6,14 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def chatlog():
     return render_template('iframe.html')
 
+@app.route('/getChat')
+def getChat():
+	return render_template('getchat.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(DEBUG=True)
 
