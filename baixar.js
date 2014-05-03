9444f03d601fba968eb0a0e1f611e993f6d3b614
Claudionor <claudiinho94@gmail.com>
2014-05-02 21:11:11 -0300

    added button to messages

--- baixar.js
+++ baixar.js
@@ -1,1 +1,17 @@
-alert("ola mundo");
+function download(){
+	alert("Hello");
+}
+
+button=document.createElement("span");
+button.setAttribute("class", "uiButtonGroupItem buttonItem");
+link=document.createElement("a");
+link.setAttribute("aria-label","Download entire conversation");
+link.setAttribute("class","uiButtonNoText uiButton uiButtonOverlay");
+link.setAttribute("href","#");
+link.setAttribute("role","button");
+link.setAttribute("onclick","download()");
+image=document.createElement("i");
+image.setAttribute("class","mrs img sp_8dfw2k sx_606396");
+link.appendChild(image);
+button.appendChild(link);
+document.getElementsByClassName("uiButtonGroup rfloat _ohf uiButtonGroupOverlay")[0].appendChild(button);
